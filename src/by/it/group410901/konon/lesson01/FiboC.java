dae75376dfa437d09f41659996de78349880b0a0
srtepa <stepankonon1@gmail.com>
2026-01-06 15:23:02 +0300

    Auto-merge PR #632 (Automated)
    * lesson 15 (изменил производительность в тесте C)
    
    * добавил незакоммитенные файлы

--- src/by/it/group410901/konon/lesson01/FiboC.java
+++ src/by/it/group410901/konon/lesson01/FiboC.java
@@ -12,7 +12,7 @@ public class FiboC {
 
     public static void main(String[] args) {
         FiboC fibo = new FiboC();
-        int n = 55555;
+        long n = 55555;
         int m = 1000;
         System.out.printf("fasterC(%d)=%d \n\t time=%d \n\n", n, fibo.fasterC(n, m), fibo.time());
     }
@@ -22,11 +22,37 @@ public class FiboC {
     }
 
     long fasterC(long n, int m) {
-        //Интуитивно найти решение не всегда просто и
-        //возможно потребуется дополнительный поиск информации
-        return -1L;
+        if (n <= 1) return n;
+
+        long prev = 0;
+        long curr = 1;
+        long period = 0;
+
+        for (int i = 0; i < m * 6; i++) {
+            long temp = (prev + curr) % m;
+            prev = curr;
+            curr = temp;
+
+            if (prev == 0 && curr == 1) {
+                period = i + 1;
+                break;
+            }
+        }
+
+        if (period > 0) {
+            n = n % period;
+        }
+
+        if (n <= 1) return n;
+
+        prev = 0;
+        curr = 1;
+        for (int i = 0; i < n - 1; i++) {
+            long temp = (prev + curr) % m;
+            prev = curr;
+            curr = temp;
+        }
+
+        return curr;
     }
-
-
 }
-
